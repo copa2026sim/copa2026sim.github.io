761b003a2fb8faa3f2c2dc9e7d728d50c87a7951
copa2026sim <hrqnoronha@gmail.com>
2026-04-03 16:53:20 -0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'copa2026-v1';
+const CACHE = 'copa2026-v2';
 const ASSETS = [
   './',
   './index.html',
@@ -18,12 +18,12 @@ self.addEventListener('activate', e => {
 
 self.addEventListener('fetch', e => {
   e.respondWith(
-    caches.match(e.request).then(r => r || fetch(e.request).then(res => {
+    fetch(e.request).then(res => {
       if (res.ok && e.request.url.startsWith('http')) {
         const clone = res.clone();
         caches.open(CACHE).then(c => c.put(e.request, clone));
       }
       return res;
-    }).catch(() => caches.match('./index.html')))
+    }).catch(() => caches.match(e.request).then(r => r || caches.match('./index.html')))
   );
 });
